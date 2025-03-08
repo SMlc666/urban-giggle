02ee4d460a2a13bab604e6f512299b0fd5dd8ab5
SHENMO <qq1030452538@outlook.com>
2025-03-08 12:53:33 +0800

    style: 增加static关键字并优化格式

--- app/src/main/cpp/src/Test/Test.cpp
+++ app/src/main/cpp/src/Test/Test.cpp
@@ -1,6 +1,7 @@
 //
 // Created by qq103 on 2025/3/7.
 //
+
 #ifdef _DEBUG_
 #include "Test.hpp"
 #include "Init/Init.hpp"
@@ -8,11 +9,15 @@
 #include "MemTool/MemTool.hpp"
 #include <thread>
 
-Init init_Test("init-Test", []() {
-  TOOLBOX_LOG_D("Test init");
-  std::thread([]() {
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    void *moduleBase = MemTool::getModuleBase<void *>("libminecraftpe.so");
-    TOOLBOX_LOG_D("libminecraftpe.so base address: %p", moduleBase);
-  }).detach(); }, 1);
+static Init init_Test(
+    "init-Test",
+    []() {
+      TOOLBOX_LOG_D("Test init");
+      std::thread([]() {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        void *moduleBase = MemTool::getModuleBase<void *>("libminecraftpe.so");
+        TOOLBOX_LOG_D("libminecraftpe.so base address: %p", moduleBase);
+      }).detach();
+    },
+    1);
 #endif
