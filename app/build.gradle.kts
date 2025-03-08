630b8da0e489e29b756431bcc4dee81078678f63
SHENMO <qq1030452538@outlook.com>
2025-03-08 12:53:22 +0800

    feat(ui): 添加Jetpack Compose支持和优化编译配置

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.compose.compiler)
 }
 
 android {
@@ -36,7 +37,19 @@ android {
         debug {
             externalNativeBuild {
                 cmake {
-                    arguments("-D_DEBUG_:BOOL=ON")
+                    cppFlags.add("-g")
+                    cFlags.add("-g")
+                    cppFlags.add("-D_DEBUG_")
+                    cFlags.add("-D_DEBUG_")
+                }
+            }
+        }
+        release {
+            externalNativeBuild {
+                cmake {
+                    cppFlags.add("-hidde")
+                    cppFlags.add("-O3")
+                    cFlags.add("-O3")
                 }
             }
         }
@@ -57,6 +70,7 @@ android {
     buildFeatures {
         viewBinding = true
         prefab = true
+        compose = true
     }
 }
 
@@ -70,4 +84,12 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     compileOnly(files("lib/XposedBridgeAPI-89.jar"))
     implementation(libs.shadowhook)
+    //compose
+    val composeBom = platform("androidx.compose:compose-bom:2025.02.00")
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
+    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    implementation("androidx.activity:activity-compose:1.10.0")
 }
